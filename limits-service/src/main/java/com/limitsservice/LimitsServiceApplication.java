cdfe7325de5563857bc7d4d76d51fcf2fda91975
Gabriel Robaina <49736266+gpr-indevelopment@users.noreply.github.com>
2020-10-24 12:35:38 -0300

    Limits-service reading from config server

--- limits-service/src/main/java/com/limitsservice/LimitsServiceApplication.java
+++ limits-service/src/main/java/com/limitsservice/LimitsServiceApplication.java
@@ -6,6 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LimitsServiceApplication {
 
+	// O limits service pega as configurações do config server na inicialização!
+	// Ou seja, quando uma configuração muda, o config server fica automaticamente atualizado, mas as aplicações que o consomem não!
+	// Verificar uma propriedade que renove a configuração lendo do config server periodicamente
 	public static void main(String[] args) {
 		SpringApplication.run(LimitsServiceApplication.class, args);
 	}
