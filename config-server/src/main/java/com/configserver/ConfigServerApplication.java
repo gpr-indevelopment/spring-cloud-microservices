267b2ffbbc0b988b868c6299509e3dd0c8e7f5f6
Gabriel Robaina <49736266+gpr-indevelopment@users.noreply.github.com>
2020-10-24 12:30:30 -0300

    Connected config server to local git

--- config-server/src/main/java/com/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/com/configserver/ConfigServerApplication.java
@@ -2,10 +2,16 @@ package com.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
 @SpringBootApplication
 public class ConfigServerApplication {
 
+	// Consultar valores das configurações!
+	// http://localhost:8888/limits-service/default
+	// http://localhost:8888/limits-service/qa
+	// http://localhost:8888/limits-service/dev
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigServerApplication.class, args);
 	}
